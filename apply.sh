c7e22dd8f848d610723add883594e78c00a98940
Pylogmon <pylogmon@outlook.com>
2023-07-24 11:03:46 +0800

    Update

--- apply.sh
+++ apply.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cp -r ./hypr ~/.config
-cp -r ./mako ~/.config
-cp -r ./tofi ~/.config
-cp -r ./waybar ~/.config
-cp -r ./swaylock ~/.config
+cp -r ./* ~/.config
+# cp -r ./mako ~/.config
+# cp -r ./tofi ~/.config
+# cp -r ./waybar ~/.config
+# cp -r ./swaylock ~/.config
 
 chmod +x ~/.config/hypr/scripts/*
 makoctl reload
